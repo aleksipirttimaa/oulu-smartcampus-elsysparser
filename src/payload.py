9e73d45db8770a850c07be3998978e5e5a3f8517
aleksipirttimaa <aleksi.pirttimaa@oulu.fi>
2020-09-21 13:41:30 +0300

    Cleaned up payload.py

--- src/payload.py
+++ src/payload.py
@@ -15,69 +15,79 @@ def temperature(bite):
     '''
     Unit is Celsius [°C].
     '''
-    return { "temperature": float_from_dual_bite(bite) / 10 }
+    return {"temperature": float_from_dual_bite(bite) / 10}
 
 def humidity(bite):
     '''
     Unit is relative humidity [%].
     '''
-    return { "humidity": bite[0] }
+    return {"humidity": bite[0]}
 
 def light(bite):
     '''
     Unit is Lux.
     '''
-    return { "light": float_from_dual_bite(bite) }
+    return {"light": float_from_dual_bite(bite)}
 
 def motion(bite):
     '''
+    ERS-Co2, ERS-Sound
     Unit depends on sensor config, as of 2020-06
-    all sensors have been configured in a unified way,
+    these sensors have been configured in a unified way,
     where the count is incremented by one for every 30
     second window any motion is detected.
     As a reporting interval of 15 minutes is used, the
     range is 0 - 30.
+    ELT-2
+    This measurement is based on the accelerometer, as these sensors are
+    missing PIR
     '''
-    return { "motion": bite[0] }
+    return {"motion": bite[0]}
 
 def co2(bite):
     '''
     Unit is ppm.
     '''
-    return { "co2": float_from_dual_bite(bite) }
+    return {"co2": float_from_dual_bite(bite)}
 
 def battery(bite):
     '''
     Unit is volts [V].
     '''
-    return { "battery": float_from_dual_bite(bite) / 1000 }
+    return {"battery": float_from_dual_bite(bite) / 1000}
 
 def sound(bite):
     '''
     Unit is Decibel [dB].
     '''
-    return { "sound_peak": bite[0], "sound_avg": bite[1] }
+    return {"sound_peak": bite[0], "sound_avg": bite[1]}
 
 def acceleration(bite):
     '''
-    Unit is G.
+    Unit is [g].
     '''
     output = []
     for value in bite:
         if value > 127:
             value -= 256
         output.append(round((value/63), 2))
-    return { "acceleration": { "x": output[0] , "y": output[1], "z": output[2] } }
+    return {"acceleration":
+                {
+                    "x": output[0],
+                    "y": output[1],
+                    "z": output[2]
+                }
+            }
 
 def pressure(bite):
     '''
-    Unit converted to bar.
+    Unit is [bar].
     '''
-    sum = 0
+    count = 0
     for index, value in enumerate(reversed(bite)):
         mid_sum = value * (256 ** index)
-        sum += mid_sum
-    return { "pressure": round(sum / (10 ** 6), 4) }
+        count += mid_sum
+    return {"pressure": round(sum / (10 ** 6), 4)}
 
 def moisture(bite):
     '''
@@ -85,13 +95,13 @@ def moisture(bite):
     ***Some sensor give value ~ 4350 for some reason, figuring it out. Maybe
     EC-5/HS-10 variation or wrong senor in settings causes this?
     '''
-    return { "moisture": float_from_dual_bite(bite)}
+    return {"moisture": float_from_dual_bite(bite)}
 
-def debug(bite):
+def debug(_bite):
     '''
     Debug frames shouldn't prevent parsing.
     '''
-    return { "debug": "not implemented" }
+    return {"debug": "not implemented"}
 
 # 'STYPE' data type id: handler, length
 STYPES = {
@@ -99,11 +109,11 @@ STYPES = {
     0x02: (humidity, 1),
     0x03: (acceleration, 3),
     0x04: (light, 2),
-    0x05: (motion, 1),
+    0x05: (motion, 1), # pir
     0x06: (co2, 2),
     0x07: (battery, 2),
     0x08: (moisture, 2),
-    0x0f: (motion, 1),
+    0x0f: (motion, 1), # accelerometer movements
     0x14: (pressure, 4),
     0x15: (sound, 2),
     0x3d: (debug, 4)
@@ -129,7 +139,6 @@ class PayloadError(Exception):
     Raised typically when the parser needs to stop, as it won't
     be able to produce a reliable output.
     '''
-    pass
 
 class Payload:
     '''
@@ -160,7 +169,9 @@ class Payload:
                 for _ in range(nob):
                     offset_b.append(self._unparsed.pop(0))
                 if offset_b != []:
-                    raise PayloadError("Sensor payload contained offset, but that is not supported.")
+                    raise PayloadError(
+                        "Sensor payload contained offset, but that is not supported."
+                        )
             elif stype_b == 0x00:
                 raise PayloadError("STYPE is null")
             elif stype_b == STYPE_SETTINGS:
